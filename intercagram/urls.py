2bebfaa9b6e1ff25d76c7f705853879fb0c7a739
chuda <chudacontreras@gmail.com>
2020-05-05 20:57:43 -0400

    metodo response

--- intercagram/urls.py
+++ intercagram/urls.py
@@ -8,7 +8,10 @@ from intercagram import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('hello-world/', views.hello_world),
+    #path('hello-world/', views.hello_world),
     #path('server-time/', views.server_time),
-    path('hi/', views.hi),
+    #path('hi/', views.hi),
+    path('hello-world/', views.hello_world),
+    path('sorted/', views.sort_integers),
+    path('hi/<str:name>/<int:age>/', views.say_hi),
 ]
--- intercagram/views.py
+++ intercagram/views.py
@@ -1,25 +1,37 @@
-#Django
+# Django
 from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
-
-
+import json
 
 
 def hello_world(request):
-    """Return a greeting."""
-    return HttpResponse('Hello, world!')
-
-def server_time(request):
     """Return a greeting."""
     return HttpResponse('Oh, hi! Current server time is {now}'.format(
         now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     ))
 
-def hi(request):
-    """Hi."""
-    #import pdb; pdb.set_trace()
-    #return HttpResponse('Hi!')
-    numbers = request.GET['numbers']
-    return HttpResponse(str(numbers))
+
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted successfully.'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+
+def say_hi(request, name, age):
+    """Return a greeting."""
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
