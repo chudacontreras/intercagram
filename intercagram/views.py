8cee76c59d9fc561d94601f90a8b8424358ec684
chuda <chudacontreras@gmail.com>
2020-05-06 09:17:54 -0400

    jugando con el response

--- intercagram/views.py
+++ intercagram/views.py
@@ -33,5 +33,5 @@ def say_hi(request, name, age):
     if age < 12:
         message = 'Sorry {}, you are not allowed here'.format(name)
     else:
-        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+        message = 'Hello, {}! Welcome to Intercagram'.format(name)
     return HttpResponse(message)
