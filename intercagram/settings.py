6eab599b77995b09526da478a72ad7b12aba46d6
chuda <chudacontreras@gmail.com>
2020-05-18 15:37:24 -0400

    agregando el middleware al settings

--- intercagram/settings.py
+++ intercagram/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'intercagram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'intercagram.urls'
